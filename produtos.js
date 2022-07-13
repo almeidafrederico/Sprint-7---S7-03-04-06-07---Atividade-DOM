465d43850e85c4806984363ec8268c4f42840dc9
Frederico S Almeida <almeidaa.frederico@gmail.com>
2022-07-13 15:28:48 -0300

    S7-06 Finalizado

--- produtos.js
+++ produtos.js
@@ -3,4 +3,32 @@ let produto = {
     valor: 3000.00,
     liquidacao: true,
     distribuidor: 'lojadakenzie.com.br'
-  }
+}
+
+
+let tarefas = [
+
+];
+
+let tasks = [
+  {
+    titulo: "Comprar comida para o gato",
+    tipo: "Urgente"
+  },
+  {
+    titulo: "Limpar o quarto",
+    tipo: "urgente"
+  },  
+  {
+    titulo: "Consertar Computador",
+    tipo: "prioritário"
+  },  
+  {
+    titulo: "Guardar dinheiro do lanche",
+    tipo: "Urgente"
+  },  
+  {
+    titulo: "Beber água",
+    tipo: "prioritário"
+  }    
+]
--- script.js
+++ script.js
@@ -29,42 +29,78 @@
 ////////////////////////////////////////////////////////////
 // 💪 Atividade - 🧠 Praticando Objeto Evento
 
-let mainTag = document.querySelector("main")
+// let mainTag = document.querySelector("main")
 
-let ulProdutos      = document.createElement("ul");
-let liProduto       = document.createElement("li");
+// let ulProdutos      = document.createElement("ul");
+// let liProduto       = document.createElement("li");
 
-let nomeTag         = document.createElement("p");
-let valorTag        = document.createElement("p");
-let liquidacaoTag   = document.createElement("p");
-let valorPromoTag   = document.createElement("p");
-let distribuidor    = document.createElement("p");
+// let nomeTag         = document.createElement("p");
+// let valorTag        = document.createElement("p");
+// let liquidacaoTag   = document.createElement("p");
+// let valorPromoTag   = document.createElement("p");
+// let distribuidor    = document.createElement("p");
 
-nomeTag.classList.add("nome");
-valorTag.classList.add("valor");
-liquidacaoTag.classList.add("liquidacao");
-valorPromoTag.classList.add("valorPromo");
-distribuidor.classList.add("distribuidor");
+// nomeTag.classList.add("nome");
+// valorTag.classList.add("valor");
+// liquidacaoTag.classList.add("liquidacao");
+// valorPromoTag.classList.add("valorPromo");
+// distribuidor.classList.add("distribuidor");
 
-nomeTag.innerText       = produto.nome;
-valorTag.innerText      = produto.valor.toFixed(2);
-liquidacaoTag.innerText = "Em liquidação";
-valorPromoTag.innerText = `Hoje apenas: ${(((produto.valor*100)*0.7)/100).toFixed(2)}`;
-distribuidor.innerText  = `Fabricante:  ${produto.distribuidor}`;
+// nomeTag.innerText       = produto.nome;
+// valorTag.innerText      = produto.valor.toFixed(2);
+// liquidacaoTag.innerText = "Em liquidação";
+// valorPromoTag.innerText = `Hoje apenas: ${(((produto.valor*100)*0.7)/100).toFixed(2)}`;
+// distribuidor.innerText  = `Fabricante:  ${produto.distribuidor}`;
 
-liProduto.append(nomeTag, valorTag, liquidacaoTag, valorPromoTag, distribuidor);
-ulProdutos.appendChild(liProduto);
-mainTag.appendChild(ulProdutos);
+// liProduto.append(nomeTag, valorTag, liquidacaoTag, valorPromoTag, distribuidor);
+// ulProdutos.appendChild(liProduto);
+// mainTag.appendChild(ulProdutos);
 
-liProduto.addEventListener('click', function(e) {
-    let targetTag = e.target;
-    if(targetTag.classList[0]=="liquidacao"){  
-        console.log(targetTag.classList[0]);
-        if(valorPromoTag.classList.contains("valorPromo")){
-            valorPromoTag.classList.replace("valorPromo", "valorPromo2");
-        }
-        else{
-            valorPromoTag.classList.replace("valorPromo2", "valorPromo");
-        }
+// liProduto.addEventListener('click', function(e) {
+//     let targetTag = e.target;
+//     if(targetTag.classList[0]=="liquidacao"){  
+//         console.log(targetTag.classList[0]);
+//         if(valorPromoTag.classList.contains("valorPromo")){
+//             valorPromoTag.classList.replace("valorPromo", "valorPromo2");
+//         }
+//         else{
+//             valorPromoTag.classList.replace("valorPromo2", "valorPromo");
+//         }
+//     }
+// });
+
+
+//////////////////////////////////////////////////
+//S7-06| 💪 Atividade: Lista de Tarefas
+
+let mainTag         = document.querySelector("main");
+
+let tarefasTag      = document.createElement("h1");
+tarefasTag.innerText= "Tarefas"
+mainTag.append(tarefasTag);
+
+let ulListaTarefa   = document.createElement("ul");
+
+for(let i in tasks){
+    if(tasks[i].tipo.toLowerCase() === "urgente"){
+        let liTarefa        = document.createElement("li");
+        liTarefa.innerText  = `${tasks[i].titulo}`;
+        let buttonTag       = document.createElement("button")
+        buttonTag.innerText = "EXCLUIR"
+        liTarefa.appendChild(buttonTag);
+        ulListaTarefa.appendChild(liTarefa);
     }
-});
+}
+
+for(let i in tasks){
+    if(tasks[i].tipo.toLowerCase() === "prioritário"){
+        let liTarefa        = document.createElement("li");
+        liTarefa.innerText  = `${tasks[i].titulo}`;
+        let buttonTag       = document.createElement("button")
+        buttonTag.innerText = "EXCLUIR"
+        liTarefa.appendChild(buttonTag);
+        ulListaTarefa.appendChild(liTarefa);
+    }
+}
+
+mainTag.appendChild(ulListaTarefa);
